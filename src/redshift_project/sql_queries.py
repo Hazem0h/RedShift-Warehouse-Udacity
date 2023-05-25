bb515b2cb5782d4d2d06f7b9e8cb75e19084f46c
Hazem Khairy <hazem.hassan.kh@gmail.com>
2023-05-26 02:33:16 +0300

    Add queries for the staging tables

--- src/redshift_project/sql_queries.py
+++ src/redshift_project/sql_queries.py
@@ -1,5 +1,8 @@
 import configparser
 
+# Constants
+STAGING_TABLE_NAME_EVENT = "staging_events"
+STAGING_TABLE_NAME_SONG = "staging_songs"
 
 
 # CONFIG
@@ -8,6 +11,8 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
+staging_events_table_drop = f"DROP TABLE IF EXISTS {STAGING_TABLE_NAME_EVENT}"
+staging_songs_table_drop = f"DROP TABLE IF EXISTS {STAGING_TABLE_NAME_SONG}"
 songplay_table_drop = ""
 user_table_drop = ""
 song_table_drop = ""
@@ -16,14 +21,59 @@ time_table_drop = ""
 
 # CREATE TABLES
 
-staging_events_table_create= (f"""
-""")
+staging_events_table_create= f"""
+    CREATE TABLE IF NOT EXISTS {STAGING_TABLE_NAME_EVENT}(
+        //event ID
+        eventId             IDENTITY(0,1)
+
+        // user data
+        userId              INT,
+        firstName           VARCHAR(50),
+        lastName            VARCHAR(50),
+        gender              VARCHAR(1),
+        level               VARCHAR(50),
+
+        // session data
+        location            VARCHAR(50),
+        userAgent           VARCHAR(50),
+        auth                VARCHAR(50),
+        sessionId           INT,
+
+        // request info
+        ts                  TIMESTAMP   sortkey, //because we want the latest timestamp to update the user level.
+        itemInSession       INT,
+        method              VARCHAR(50),
+        status              INT,
+        page                VARCHAR(50),
+
+        // song data
+        song                VARCHAR(50),
+        artist              VARCHAR(50),
+        length              REAL,
+        registration        TIMESTAMP
+    );
+"""
 # ?
 # I don't know about `registration` What does it represent?
 # song registration in the dataset?
 
-staging_songs_table_create = (f"""
-""")
+staging_songs_table_create = f"""
+    CREATE TABLE IF NOT EXISTS {STAGING_TABLE_NAME_SONG}(
+        //song data
+        song_id             VARCHAR(50) PRIMARY KEY,
+        num_songs           INT,
+        title               VARCHAR(50),
+        duration            REAL,
+        year                INT,
+
+        // artist data
+        artist_id           VARCHAR(50),
+        artist_latitude     VARCHAR(50), 
+        artist_longitude    VARCHAR(50), 
+        artist_location     VARCHAR(50), 
+        artist_name         VARCHAR(50)
+    );
+"""
 
 songplay_table_create = ("""
 """)
@@ -42,11 +92,15 @@ time_table_create = ("""
 
 # STAGING TABLES
 
-staging_events_copy = (f"""
-""").format()
+staging_events_copy = f"""
+    COPY {STAGING_TABLE_NAME_EVENT} FROM {config["S3"]["LOG_DATA"]}
+    CREDENTIALS {config['IAM_ROLE']['ARN']}
+"""
 
-staging_songs_copy = ("""
-""").format()
+staging_songs_copy = f"""
+    COPY {STAGING_TABLE_NAME_SONG} FROM {config["S3"]["SONG_DATA"]}
+    CREDENTIALS {config['IAM_ROLE']['ARN']}
+"""
 
 # FINAL TABLES
 
