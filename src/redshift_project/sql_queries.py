4022d25e7669bb59e35979fe6d79476a4f3bb210
Hazem Khairy <hazem.hassan.kh@gmail.com>
2023-05-26 08:20:00 +0300

    Table Creation queries first draft

--- src/redshift_project/sql_queries.py
+++ src/redshift_project/sql_queries.py
@@ -3,7 +3,11 @@ import configparser
 # Constants
 STAGING_TABLE_NAME_EVENT = "staging_events"
 STAGING_TABLE_NAME_SONG = "staging_songs"
-
+TABLE_NAME_SONGPLAY = "songplays"
+TABLE_NAME_USER = "users"
+TABLE_NAME_SONG = "songs"
+TABLE_NAME_ARTIST = "artists"
+TABLE_NAME_TIME = "time"
 
 # CONFIG
 config = configparser.ConfigParser()
@@ -13,11 +17,11 @@ config.read('dwh.cfg')
 
 staging_events_table_drop = f"DROP TABLE IF EXISTS {STAGING_TABLE_NAME_EVENT}"
 staging_songs_table_drop = f"DROP TABLE IF EXISTS {STAGING_TABLE_NAME_SONG}"
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = f"DROP TABLE IF EXISTS {TABLE_NAME_SONGPLAY}"
+user_table_drop = f"DROP TABLE IF EXISTS {TABLE_NAME_USER}"
+song_table_drop = f"DROP TABLE IF EXISTS {TABLE_NAME_SONG}"
+artist_table_drop = f"DROP TABLE IF EXISTS {TABLE_NAME_ARTIST}"
+time_table_drop = f"DROP TABLE IF EXISTS {TABLE_NAME_TIME}"
 
 # CREATE TABLES
 
@@ -40,7 +44,7 @@ staging_events_table_create= f"""
         sessionId           INT,
 
         // request info
-        ts                  TIMESTAMP   sortkey, //because we want the latest timestamp to update the user level.
+        ts                  TIMESTAMP       SORTKEY, //because we want the latest timestamp to update the user level.
         itemInSession       INT,
         method              VARCHAR(50),
         status              INT,
@@ -56,11 +60,12 @@ staging_events_table_create= f"""
 # ?
 # I don't know about `registration` What does it represent?
 # song registration in the dataset?
+# or user registration in the app?
 
 staging_songs_table_create = f"""
     CREATE TABLE IF NOT EXISTS {STAGING_TABLE_NAME_SONG}(
         //song data
-        song_id             VARCHAR(50) PRIMARY KEY,
+        song_id             VARCHAR(50)     PRIMARY KEY,
         num_songs           INT,
         title               VARCHAR(50),
         duration            REAL,
@@ -75,31 +80,86 @@ staging_songs_table_create = f"""
     );
 """
 
-songplay_table_create = ("""
-""")
+songplay_table_create = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_SONGPLAY}(
+        songplay_id         INT             PRIMARY KEY,
+        start_time          TIMESTAMP,
+        user_id             INT,
+        level               VARCHAR(50),
+        song_id             VARCHAR(50)     DISTKEY,
+        artist_id           VARCHAR(50),
+        session_id          INT,
+        location            VARCHAR(50),
+        user_agent          VARCHAR(50),
 
-user_table_create = ("""
-""")
+        FOREIGN KEY (start_time) REFERENCES {TABLE_NAME_TIME}(start_time),
+        FOREIGN KEY (user_id) REFERENCES {TABLE_NAME_USER}(user_id),
+        FOREIGN KEY (song_id) REFERENCES {TABLE_NAME_SONG}(song_id),
+        FOREIGN KEY (artist_id) REFERENCES {TABLE_NAME_ARTIST}(artist_id)
+    );
+"""
 
-song_table_create = ("""
-""")
 
-artist_table_create = ("""
-""")
+user_table_create = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_USER}(
+        user_id             INT             PRIMARY KEY,
+        first_name          VARCHAR(50),
+        last_name           VARCHAR(50),
+        gender              VARCHAR(1),
+        level               VARCHAR(50)
 
-time_table_create = ("""
-""")
+        DISTSTYLE AUTO
+    );
+"""
+
+song_table_create = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_SONG}(
+        song_id             VARCHAR(50)     PRIMARY KEY     DISTKEY,
+        title               VARCHAR(50),
+        artist_id           VARCHAR(50),
+        year                INT,
+        duration            REAL,
+
+        FOREIGN KEY (artist_id) REFERENCES {TABLE_NAME_ARTIST}(artist_id)
+    );
+"""
+
+artist_table_create = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_ARTIST}(
+        artist_id           VARCHAR(50)     PRIMARY KEY     DISTKEY,
+        name                VARCHAR(50),
+        location            VARCHAR(50),
+        latitude            VARCHAR(50),
+        longitude           VARCHAR(50),
+
+        DISTSTYLE AUTO
+    );
+"""
+
+time_table_create = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_NAME_TIME}(
+        start_time          TIMESTAMP       PRIMARY KEY,
+        hour                INT,
+        day                 INT,
+        week                INT,
+        month               INT,
+        year                INT,
+        weekday             INT,
+
+        DISTSTYLE AUTO
+    );
+"""
 
 # STAGING TABLES
 
 staging_events_copy = f"""
     COPY {STAGING_TABLE_NAME_EVENT} FROM {config["S3"]["LOG_DATA"]}
-    CREDENTIALS {config['IAM_ROLE']['ARN']}
+    CREDENTIALS {config['IAM_ROLE']['ARN']};
 """
 
 staging_songs_copy = f"""
     COPY {STAGING_TABLE_NAME_SONG} FROM {config["S3"]["SONG_DATA"]}
-    CREDENTIALS {config['IAM_ROLE']['ARN']}
+    CREDENTIALS {config['IAM_ROLE']['ARN']};
 """
 
 # FINAL TABLES
